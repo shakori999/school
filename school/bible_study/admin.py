ea2a01565e2ea22a4477efd1b644809186486d44
shakori999 <shakori999@gmail.com>
2023-09-16 09:42:46 +0300

    add lesson, lessonpdf to admin

--- school/bible_study/admin.py
+++ school/bible_study/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Lesson,LessonPDF
 # Register your models here.
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = (
+            'class_info',
+            'lesson_number',
+            'lesson_name',
+            'verse_to_memorize',
+            'chapters_to_read',
+            )
+
+class LessonPDFAdmin(admin.ModelAdmin):
+    list_display = (
+            'lesson',
+            'pdf_file',
+            )
+
+
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(LessonPDF, LessonPDFAdmin)
--- school/bible_study/models.py
+++ school/bible_study/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
 
-
-
 class Lesson(models.Model):
     class_info = models.ForeignKey("classes.Class", on_delete=models.CASCADE, related_name='lessons')
 
@@ -13,13 +11,11 @@ class Lesson(models.Model):
     # Fields specific to each lesson
     verse_to_memorize = models.CharField(max_length=255, blank=True, null=True)
     chapters_to_read = models.CharField(max_length=255, blank=True, null=True)
-    pdf_file = models.FileField(upload_to='lesson_pdfs/', blank=True, null=True)
 
     # Add other fields related to lessons as needed
 
     def __str__(self):
         return f"Lesson {self.lesson_number} for {self.class_info}"
-
 
 class LessonPDF(models.Model):
     lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE, related_name='pdf')
