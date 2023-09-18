5faf0e1084c9d40664fad79e8122a826a37b9fd7
shakori999 <shakori999@gmail.com>
2023-09-18 19:06:21 +0300

    tested class integrity

--- school/classes/tests/test_models_integritions.py
+++ school/classes/tests/test_models_integritions.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 from django.db import IntegrityError, transaction
 
 from ..models import Class
+from ...attendance.models import Attendance
+from ...student.models import Student
 
 '''
 this section for testing class model
@@ -41,3 +43,66 @@ def test_unique_class_title(course, cycle, course_per_cycle):
 
     # Ensure that the IntegrityError is raised
     assert isinstance(e.value, IntegrityError)
+
+@pytest.mark.django_db
+def test_course_foreign_key_integrity():
+    # Test that the foreign key relationship with the Course model is enforced properly
+    with pytest.raises(IntegrityError):
+        Class.objects.create(
+            classno=1,
+            classtitle="Test Class",
+            classdate=timezone.now().date(),
+            starttime=timezone.now().time(),
+            endtime=(timezone.now() + timezone.timedelta(hours=1)).time(),
+            course=None  # Use an invalid course object
+        )
+
+@pytest.mark.django_db
+def test_cycle_foreign_key_integrity():
+    # Test that the foreign key relationship with the Cycle model is enforced properly
+    with pytest.raises(IntegrityError):
+        Class.objects.create(
+            classno=2,
+            classtitle="Test Class",
+            classdate=timezone.now().date(),
+            starttime=timezone.now().time(),
+            endtime=(timezone.now() + timezone.timedelta(hours=1)).time(),
+            cycle=None  # Use an invalid cycle object
+        )
+
+@pytest.mark.django_db
+def test_courses_per_cycle_foreign_key_integrity():
+    # Test that the foreign key relationship with the CoursesPerCycle model is enforced properly
+    with pytest.raises(IntegrityError):
+        Class.objects.create(
+            classno=3,
+            classtitle="Test Class",
+            classdate=timezone.now().date(),
+            starttime=timezone.now().time(),
+            endtime=(timezone.now() + timezone.timedelta(hours=1)).time(),
+            coursespercycle=None  # Use an invalid courses_per_cycle object
+        )
+
+@pytest.mark.django_db
+def test_students_many_to_many_relationship(student,course, course_per_cycle, cycle):
+    # Test that the many-to-many relationship with the Student model through the Attendance model is working as expected
+    class_obj = Class.objects.create(
+        classno=4,
+        classtitle="Test Class",
+        classdate=timezone.now().date(),
+        starttime=timezone.now().time(),
+        endtime=(timezone.now() + timezone.timedelta(hours=1)).time(),
+        course=course,
+        cycle=cycle,
+        coursespercycle=course_per_cycle,
+    )
+    # Create an attendance record to associate the student with the class
+    Attendance.objects.create(
+            class_info=class_obj,
+            student=student,
+            timearrive=timezone.now().time(),
+            timeleave=(timezone.now() + timezone.timedelta(hours=1)).time(),
+            course=course,
+            cycle=cycle,
+            )
+    assert class_obj.students.count() == 1  # Check if the student is associated with the class
