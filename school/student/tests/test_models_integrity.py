29fd328055638f693cda82e1bd78afecafd42ea8
shakori999 <shakori999@gmail.com>
2023-09-18 19:26:53 +0300

    tested Enrollment integrity

--- school/student/tests/test_models_integrity.py
+++ school/student/tests/test_models_integrity.py
@@ -1,7 +1,12 @@
 import pytest
+from django.utils import timezone
 from django.db import IntegrityError
-from django.test import TestCase
-from ..models import Student
+from ..models import Student, Enrollment
+
+
+'''
+this section for testing Student model
+'''
 
 @pytest.mark.django_db
 def test_student_name_unique(create_student_role):
@@ -69,3 +74,63 @@ def test_user_creation(create_student_role):
     )
     student.save()  # This should trigger the creation of corresponding Person and User instances
     assert student.user is not None  # Ensure that the user field is populated
+
+'''
+this section for testing Enrollemnt model
+'''
+@pytest.mark.django_db
+def test_course_per_cycle_foreign_key_integrity(student, course_per_cycle):
+    # Create an enrollment with a valid student and course_per_cycle
+    enrollment = Enrollment.objects.create(
+        course_per_cycle=course_per_cycle,
+        student=student,
+        enrollmentdate=timezone.now().date(),
+        cancelled=False,
+        cancellationreason="",
+
+    )
+    # Retrieve the created enrollment and check its course_per_cycle foreign key
+    retrieved_enrollment = Enrollment.objects.get(id=enrollment.id)
+    assert retrieved_enrollment.course_per_cycle == course_per_cycle
+
+@pytest.mark.django_db
+def test_student_foreign_key_integrity(student,course_per_cycle):
+    # Create an enrollment with a valid student and course_per_cycle
+    enrollment = Enrollment.objects.create(
+        course_per_cycle=course_per_cycle,
+        student=student,
+        enrollmentdate=timezone.now().date(),
+        cancelled=False,
+        cancellationreason="",
+
+    )
+
+    # Retrieve the created enrollment and check its student foreign key
+    retrieved_enrollment = Enrollment.objects.get(id=enrollment.id)
+    assert retrieved_enrollment.student == student
+
+@pytest.mark.django_db
+def test_studnet_foreign_key_not_provide_integrity(student, course_per_cycle):
+    # Create an enrollment with a valid student and course_per_cycle
+    with pytest.raises(IntegrityError):
+        enrollment = Enrollment.objects.create(
+            course_per_cycle=course_per_cycle,
+            enrollmentdate=timezone.now().date(),
+            cancelled=False,
+            cancellationreason="",
+
+        )
+        enrollment.full_clean()
+
+@pytest.mark.django_db
+def test_course_per_cycle_foreign_key_not_provide_integrity(student, course_per_cycle):
+    # Create an enrollment with a valid student and course_per_cycle
+    with pytest.raises(IntegrityError):
+        enrollment = Enrollment.objects.create(
+            enrollmentdate=timezone.now().date(),
+            student=student,
+            cancelled=False,
+            cancellationreason="",
+
+        )
+        enrollment.full_clean()
